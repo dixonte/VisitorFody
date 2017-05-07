2bcae0c89770876ef29599b51ab55c2207b1aabe
T E Dixon <dixonte@gmail.com>
2017-05-07 14:00:09 +1000

    Documentation comments

---
+++ FodyVisitorTest/DomainModel/Visitors/DelegateDuck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FodyVisitorTest.DomainModel.Visitors
+{
+    public class DelegateDuck
+    {
+        public Action<Employee> Employee { get; set; }
+        public Action<Director> Director { get; set; }
+
+        public void Visit(Director director)
+        {
+            director.DontPlayGolf();
+        }
+    }
+}
--- FodyVisitorTest/Program.cs
+++ FodyVisitorTest/Program.cs
@@ -25,27 +25,42 @@ namespace FodyVisitorTest
 
             int employeeCountByVisitor = 0;
 
-            var anon = new
+            // Since ConcreteVisitor already implements IPersonVisitor, these statements should generate the same IL
+            var concreteVisitor = VisitorFactory<IPersonVisitor>.Create(new ConcreteVisitor());
+            concreteVisitor = new ConcreteVisitor();
+
+            // Anonymous classes provide their implementations by way of Action<> delegates
+            // The name of the properties is not important
+            var anonymousVisitor = VisitorFactory<IPersonVisitor>.Create(new
             {
                 Employee = new Action<Employee>(e => employeeCountByVisitor++),
                 Director = new Action<Director>(d =>
                 {
                     d.PlayGolf();
                 })
-            };
+            }, ActionOnMissing.NoOp);
 
-            var anonymousVisitor = VisitorFactory<IPersonVisitor>.Create(anon, ActionOnMissing.NoOp);
-            var concreteVisitor = VisitorFactory<IPersonVisitor>.Create(new ConcreteVisitor());
+            // Concrete classes can also provide implementations by way of Action<> delegates
+            // Use caution, as any properties of type Action<> that match a Visit method in the given interface will be wired up as implementation
             var concreteDelegate = VisitorFactory<IPersonVisitor>.Create(new ConcreteDelegate()
             {
                 Employee = new Action<Employee>(e => employeeCountByVisitor++)
             });
+
+            // If for some reason you want to, you can use VisitorFactory to inject visitor interfaces into concrete classes with methods
+            // At the moment, this will take the first method that matches the required signature, regardless of name.
+            // This will probably change in future.
             var concreteDuck = VisitorFactory<IPersonVisitor>.Create(new ConcreteDuck());
-            concreteVisitor = new ConcreteVisitor();
 
-            //anon.Employee(new Employee());
-            //anon.Director(new Director());
-
+            // In the case where a concrete class has a method and a Action<> delegate that both match, the method trumps the delegate
+            var delegateDuck = VisitorFactory<IPersonVisitor>.Create(new DelegateDuck()
+            {
+                Employee = new Action<Employee>(e => employeeCountByVisitor++),
+                Director = new Action<Director>(director =>
+                {
+                    director.PlayGolf();
+                })
+            });
 
             foreach (var person in people)
             {
